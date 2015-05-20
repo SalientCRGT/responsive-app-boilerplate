b1596e98405f92d14977bf32d2d492d1687fbb31
ttyson-CRGT <ttyson@crgt.com>
2015-05-20 15:58:23 -0400

    cg-angular template for web app

---
+++ web/app.js
@@ -0,0 +1,23 @@
+angular.module('web', ['ui.bootstrap','ui.utils','ui.router','ngAnimate']);
+
+angular.module('web').config(function($stateProvider, $urlRouterProvider) {
+
+    /* Add New States Above */
+    $urlRouterProvider.otherwise('/home');
+
+});
+
+angular.module('web').run(function($rootScope) {
+
+    $rootScope.safeApply = function(fn) {
+        var phase = $rootScope.$$phase;
+        if (phase === '$apply' || phase === '$digest') {
+            if (fn && (typeof(fn) === 'function')) {
+                fn();
+            }
+        } else {
+            this.$apply(fn);
+        }
+    };
+
+});
