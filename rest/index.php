f0765e5fabbb0fa6e7cfe0d058410ee22f84981e
lweitzel <lweitzel@crgt.com>
2015-05-21 11:40:42 -0400

    minor change in code

--- rest/index.php
+++ rest/index.php
@@ -3,9 +3,9 @@
 
 <h1>REST API</h1>
 
-<a href="findEmployees.php">findEmployees</a>
+<a href="findEmployees.php">findEmployees - mysql query returning JSON</a>
 <br>
-<a href="jsontest.php">jsontest</a>
+<a href="jsontest.php">jsontest using https://github.com/AlexisTM/Simple-Json-PHP</a>
 
 </body>
 </html>
