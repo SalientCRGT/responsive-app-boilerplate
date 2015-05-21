d5c1629d6fa8ec0ef953010a145bee67d4183473
lweitzel <lweitzel@crgt.com>
2015-05-21 12:48:23 -0400

    new sql folder

---
+++ rest/dbTest.php
@@ -0,0 +1,23 @@
+<?php
+// Set JSON output
+header('Content-Type: application/json');
+
+//open connection to mysql db
+$connection = mysqli_connect("localhost","root","root","lance") or die("Error " . mysqli_error($connection));
+
+//fetch table rows from mysql db
+$sql = "select * from tbl_employee";
+$result = mysqli_query($connection, $sql) or die("Error in Selecting " . mysqli_error($connection));
+
+//create an array
+$emparray[] = array();
+while($row =mysqli_fetch_assoc($result)) {
+   $emparray[] = $row;
+}
+echo json_encode($emparray);
+
+
+//close the db connection
+mysqli_close($connection);
+
+?>
