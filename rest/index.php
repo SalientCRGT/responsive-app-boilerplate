084163417732a97b8b6a49345fd19e5de6cb3356
lweitzel <lweitzel@crgt.com>
2015-05-26 21:38:29 -0400

    wine

--- rest/index.php
+++ rest/index.php
@@ -3,9 +3,9 @@
 
 <h1>REST API</h1>
 
-<a href="rest/findEmployees.php">findEmployees - mysql query returning JSON</a>
+<a href="/rest/findEmployees.php">findEmployees - mysql query returning JSON</a>
 <br>
-<a href="rest/jsontest.php">jsontest using https://github.com/AlexisTM/Simple-Json-PHP</a>
+<a href="/rest/jsontest.php">jsontest using https://github.com/AlexisTM/Simple-Json-PHP</a>
 
 <pre>
 <?php
